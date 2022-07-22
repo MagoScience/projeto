a0be5f547aa2f69f7f4bee5500f8ffd29a298993
Mago Henrique <magnohenriquedev@gmail.com>
2022-07-22 18:31:20 -0400

    novos codigos

---
+++ calculadoraICM.py
@@ -0,0 +1,8 @@
+# 🚨 Não altere o código abaixo 👇
+altura = input("digite sua altura em m: ")
+peso = input("digite seu peso em kg: ")
+# 🚨 Não altere o código acima 👆
+
+# Escreva seu código abaixo desta linha 👇
+bmi = int(peso) / float(altura) ** 2
+print("A sua massa corporal é de ", + int(bmi))
---
+++ geradorNomeBanda.py
@@ -0,0 +1,10 @@
+# 1. Crie uma saudação para o seu programa.
+print("Bem vindo ao Gerador de nome de banda, vamos lá!")
+# 2. Pergunte ao usuário a cidade em que ele cresceu.
+cidade = input("Cidade que você nasceu? ")
+# 3. Peça ao usuário o nome de um animal de estimação.
+pet = input("Qual o nome de um animal de estimação? ")
+# 4. Combine o nome da cidade e do animal de estimação e mostre o nome da#
+print("O nome da sua banda é " + cidade + " " + pet)
+soma = cidade + " " + pet
+print("O nome da sua banda é " + soma)
